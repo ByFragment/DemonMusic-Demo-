0945b1bae7f0815095bd1fc221e54423e771dcbb
ByFragment <81448574+ByFragment@users.noreply.github.com>
2022-02-20 21:35:56 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
     BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-    BOT_NAME = os.environ.get("BOT_NAME")
+    BOT_USERNAME = os.environ.get("BOT_NAME")
     PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME")
     PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID"))
