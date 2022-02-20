e5b3f92b9ab543058530c0c84c6c248ec39bc87f
ByFragment <81448574+ByFragment@users.noreply.github.com>
2022-02-20 21:44:08 +0400

    Update DemonMusicBot.py

--- DemonMusicBot.py
+++ DemonMusicBot.py
@@ -118,7 +118,7 @@ def a(client, message):
             secmul *= 60
         message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name, performer="@Botsinator")
         m.delete()
-        bot.send_audio(chat_id={Config.PLAYLIST_ID}, audio=audio_file, caption=rep, performer="@Botsinator", parse_mode='md', title=title, duration=dur, thumb=thumb_name)
+        bot.send_audio(chat_id=Config.PLAYLIST_ID, audio=audio_file, caption=rep, performer="@Botsinator", parse_mode='md', title=title, duration=dur, thumb=thumb_name)
     except Exception as e:
         m.edit('**⚠️ Gözlənilməyən xəta yarandı.**\n**Xahiş edirəm xətanı sahibimə xəbərdar et!**')
         print(e)
